e8856963ddd3e05f4a1f8bbfb32f39c329926ce4
arunreddy9966 <reddyarunkumar61@gmail.com>
2024-07-24 12:01:50 +0530

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,20 @@
+let heroName = "Chiranjeevi";
+
+let moviesList = [
+                "acharya",
+                "khaid no 150",
+                "vishwambhara",
+                "god father",
+                "bhola shankar",
+                "waltair veerayya",
+                "syera narasimhareddy",
+                "tagore",
+                "indra",
+                "daddy",
+               ];
+
+   let aboutChiranjeevi =()=>{
+   console.log("Chiranjeevi is an indian film actor and he is one of the successful and influential actor in the history of indian cinema.")
+               };
+
+    export {heroName,moviesList,aboutChiranjeevi};
---
+++ server.js
@@ -0,0 +1,6 @@
+import { heroName,moviesList,aboutChiranjeevi } from "./hero.js";
+
+
+console.log(heroName);
+console.log(moviesList);
+aboutChiranjeevi();
